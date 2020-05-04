acb68f7a79458ff9dea55c326cb871226ba7a416
André Faria <andrefaria-1@hotmail.com>
2020-05-04 11:30:46 -0300

    findById implementation

--- src/model/dao/DaoFactory.java
+++ src/model/dao/DaoFactory.java
@@ -1,16 +1,13 @@
 package model.dao;
 
+import db.DB;
 import model.dao.impl.SellerDaoJDBC;
 
-public class DaoFactory { // classe responsável por fabricar os DAOS. 
-	
+public class DaoFactory { // classe responsável por fabricar os DAOS.
+
 	public static SellerDao createSellerDao() {
-		return new SellerDaoJDBC(); // faz com que a implementaçao nao seja explicita no programa principal.
+		return new SellerDaoJDBC(DB.getConnection()); // faz com que a implementaçao nao seja explicita no programa
+														// principal.
 	}
-	
-	
-	
-	
-	 
 
 }
--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -1,34 +1,84 @@
 package model.dao.impl;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
+import db.DB;
+import db.DbException;
 import model.dao.SellerDao;
+import model.entities.Department;
 import model.entities.Seller;
 
-public class SellerDaoJDBC  implements SellerDao {
+public class SellerDaoJDBC implements SellerDao {
+
+	private Connection conn;
+
+	public SellerDaoJDBC(Connection conn) { // dependencia para estabelecer a conexao
+		this.conn = conn;
+	}
 
 	@Override
 	public void insert(Seller obj) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void update(Seller obj) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void deleteById(Seller obj) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public Seller findById(Integer id) {
-		// TODO Auto-generated method stub
-		return null;
+		PreparedStatement st = null;
+		ResultSet rs = null;
+
+		try {
+			st = conn.prepareStatement(
+					"SELECT seller.*,department.Name as DepName " + "FROM seller INNER JOIN department "
+							+ "ON seller.DepartmentId = department.Id " + "WHERE seller.Id = ? ");
+
+			st.setInt(1, id);
+			rs = st.executeQuery(); // armazena as alterações feitas pela lógica no rs.
+
+			if (rs.next()) { // testa se ha dados no ResultSet.
+				Department dep = new Department();
+				dep.setId(rs.getInt("DepartmentId"));
+				dep.setName(rs.getString("DepName"));
+				Seller obj = new Seller();
+				obj.setId(rs.getInt("Id"));
+				obj.setName(rs.getString("Name"));
+				obj.setEmail(rs.getString("Email"));
+				obj.setBirthDate(rs.getDate("BirthDate"));
+				obj.setBaseSalaray(rs.getDouble("BaseSalary"));
+				obj.setDepartment(dep);
+
+				return obj;
+
+			}
+
+			return null;
+
+		} catch (SQLException e) {
+			throw new DbException(e.getMessage());
+
+		} finally { // nao é necessario encerrar a conexao, pois podemos querer utilzar essa classe
+					// em situaçoes futuras.
+			DB.closeStatement(st);
+			DB.closeResultSet(rs);
+
+		}
+
 	}
 
 	@Override
@@ -36,7 +86,5 @@ public class SellerDaoJDBC  implements SellerDao {
 		// TODO Auto-generated method stub
 		return null;
 	}
-	
-	
 
 }
